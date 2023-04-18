384fba22b158ec461c56fbb45b4b664dc7ab6358
Abdujalol01 <jalolhalimov203@gmail.com>
2023-04-18 18:12:12 +0500

    add modal in js and change css styles

--- Js/main.js
+++ Js/main.js
@@ -1,11 +1,41 @@
-const elModal = document.querySelector(".modal");
-const elModalClose = document.querySelector(".js-modal-close");
-
-if(elModalClose){
-  elModalClose.addEventListener("click", function(){
-    elModal.classList.remove("modal-open");
+window.addEventListener("DOMContentLoaded" , ()=>{
+  
+  const modal = document.querySelector(".modal"),
+  modalClose = document.querySelector(".js-modal-close")
+  
+  
+  function openModal(){
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+    document.body.style.overflow="hidden";
+  }
+  function closeModal(){
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+    document.body.style.overflow="";
+  }
+  modalClose.addEventListener("click" , closeModal);
+  const showModal = setTimeout(openModal ,2000);
+  
+  modal.addEventListener("click" ,(e)=>{
+    if(e.target == modal){
+      closeModal();
+    }
   });
-}
-setTimeout(function(){
-  elModal.classList.add('modal-open')
-},3000);
+  
+  document.addEventListener("keydown" , (e)=>{
+    if(e.code=="Escape" && modal.classList.contains("show")){
+      closeModal();
+    }
+  });
+  
+  function showModalByScroll(){
+    if(window.pageYOffset + document.documentElement.clientHeight>= document.documentElement.scrollHeight){
+      openModal();
+      window.removeEventListener("scroll" , showModalByScroll);
+    }
+  }
+  
+  window.addEventListener("scroll" , showModalByScroll);
+  
+});
